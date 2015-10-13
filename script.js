e53821b634328820e4a279496c3cfa642f71267a
Matteo Bernardini <mat_teobe@yahoo.it>
2015-10-13 23:01:59 +0200

    Reworking the whole thing (not ended yet)

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 /*
-The code of the Asynchronous Modal Dialog Window, which can replace the default JavaScript dialog windows (alert(); prompt(); confirm())
-The code integrates 3 elements. The first is a global object named "dialogSettings" that you can use to modify the default behaviors of the dialog window, which are:
-- The default type: dialogSettings.defType (alert)
-- The default title: dialogSetings.defTitle (Message)
-- The default content: dialogSettings.defContent (<i>Missing text</i>)
-- The default content of the ok button: dialogSettings.okText (OK)
-- The default content of the continue button: dialogSettings.continueText (Continue)
-- The default content of the cancenl button: dialogSettings.cancelText (Cancel)
 
-The second object is the main function, called dialog() which needs the following parameters, passed as object on the first parameter:
-- type: the type of the dialog window, alert, prompt or confirm
-- title: the text which will be displayed as the title of the dialog window
-- content: the text of the content of the dialog window
-- id: is and identifier which can be get afterwards with the response of the callBack
-- vars: is an object which can be used for passing variables between multiple dialog window, and then with the final callBack which is the final function, or to remember the local variables of the calling function
-- callBack: is a string which includes the code to be executed when the dialog window is closed
+Asynchronous Modal Dialog Window, a utility which can replace the default JavaScript dialog windows (alert(); prompt(); confirm())
 
-The last object is an object that will be returned in the callBack if requested, is named returnObj and contain the following properties:
-- id: responses the identifier passed when calling the function, default to null
-- vars: is an object which contains the passed variables with the "vars" argument
-- boolean: responses the Boolean value that is true if the user has pressed OK or continue, and false for cancel
-- value: if the type of the requested dialog window is "prompt" it will contain the value inserted in the input box, instead it will be undefined.
+The program integrates 3 elements.
 
-Version: 1.0
+## The first is a global Object named `dialogSettings` that you can use to modify the default behavior of the dialog windows ##
+
+__object_key__	__type__	__default_value__		__comment__
+defType		String		"alert"				default type
+defTitle	String		"Message"			default title
+defContent	String		"<i>Missing text</i>"		default body of the dialog
+okText		String		"OK"				default content of the <ok> button
+continueText	String		"Continue"			default content of the <continue> button
+cancelText	String		"Cancel"			default content of the <cancel> button
+
+
+## The second element is the main Function, `dialog()`, which needs the following options, passed with an object as first argument ##
+
+__object_key__	__type__	__comment__
+type		String		type of the dialog window. Possible values are ['alert', 'prompt', 'confirm']
+title		String		text which will be displayed as the title of the dialog window
+content		String		body of the dialog window
+id		String		identifier which can be retrieved afterwards in the `returnObj`, on the callback function
+vars		Object		object of convenience that will be returned in the `returnObj`, on the callback function
+callback	Function	function to be called when the dialog window is closed
+
+
+## The last one is an Object that will be returned in the callback function as first argument, named `returnObj` for convenience of this guide ##
+
+__object_key__	__type__	__comment__
+id		String		identifier passed when calling the `dialog()` function, defaults to undefined
+vars		Object		same object passed with the function (NB: same reference), defaults to undefined
+action		Boolean		`true` if the user has pressed <ok> or <continue>, `false` for <cancel>
+value		String		if the type of the requested dialog window was "prompt" it will contain the value inserted in the input box, otherwise it will be undefined
+
+================================================================================================
+
+Version: 2.0.0
 Made by: Matteo Bernardini, @mttbernardini
 Shared with license CC, by-nc-sa:
 Attribution: You are free to distribute this code, download it and use it, but you must declare who's the owner.
@@ -30,121 +44,142 @@ No Commercial: You aren't allowed to use this code for commercial or lucrative p
 Share Alike: you are free to edit this code as you like, but you must include this commented part which includes the description, the version, the owner, and the license which you cannot modify.
 
 To see the integral declaration of the license see here: http://creativecommons.org/licenses/by-nc-sa/3.0/
-To have more information about the use of the code see here: http://inviodeidatistp.altervista.org/libary/dialog/
+Check this project on GitHub: http://github.com/mttbernardin/dialog
+
 */
 
-//First of all, cause IE uses attachEvent instead of addEventListener we make an unique function
-function addEvent(elm, handler, func, useCapture) {
-  if (document.addEventListener) elm.addEventListener(handler, func, useCapture);
-  else if (document.attachEvent) elm.attachEvent("on"+handler, func);
+
+
+// Backwards compatibility with IE < 9
+if (!window.addEventListener) {
+	Object.defineProperties(Element.prototype, {
+		"addEventListener": {
+			value: function(evt, handler, capture) {
+				this.attachEvent('on'+evt, handler);
+			}
+		},
+		"removeEventListener": {
+			value: function(evt, handler, capture) {
+				this.detachEvent('on'+evt, handler);
+			}
+		},
+	});
 }
-function removeEvent(elm, handler, func, useCapture) {
-  if (document.removeEventListener) elm.removeEventListener(handler, func, useCapture);
-  else if (document.detachEvent) elm.detachEvent("on"+handler, func);
-}
+
+
+// MAIN FUNCTION //
 
 function dialog(item) {
 
-//default values
-item = item || new Object();
-if (typeof(dialogSettings)=="undefined") dialogSettings = new Object();
-dialogSettings.defTitle = dialogSettings.defTitle || "Message";
-dialogSettings.defType = dialogSettings.defType || "alert";
-dialogSettings.defContent = dialogSettings.defContent || "<i>Missing text</i>";
-dialogSettings.okText = dialogSettings.okText || "OK";
-dialogSettings.continueText = dialogSettings.continueText || "Continue";
-dialogSettings.cancelText = dialogSettings.cancelText || "Cancel";
+	item = item || new Object();
+	if (typeof(dialogSettings)=="undefined") dialogSettings = new Object();
 
-//Switch between default values or passed values
-item.title = item.title || dialogSettings.defTitle;
-item.type = item.type || dialogSettings.defType;
-item.content = item.content || dialogSettings.defContent;
-item.defValue = item.defValue || null;
-item.callBack = item.callBack || null;
-item.vars = item.vars || null;
-item.id = item.id || null;
-var inputtag = document.createElement("input");
-inputtag.setAttribute("type", "text");
-var structure = '<div data-type="inner"><div data-type="title">Titolo del popup</div><div data-type="content"><div data-type="text">Contenuto</div><div data-type="prompt">'+inputtag.outerHTML+'</div><div data-type="actions"><button type="button" data-type="ok">'+dialogSettings.okText+'</button><button type="button" data-type="cancel">'+dialogSettings.cancelText+'</button></div></div></div>';
+	// == Default Values == //
+	dialogSettings.defTitle		=	dialogSettings.defTitle		||  "Message";
+	dialogSettings.defType		=	dialogSettings.defType		||  "alert";
+	dialogSettings.defContent	=	dialogSettings.defContent	||  "<i>Missing text</i>";
+	dialogSettings.okText		=	dialogSettings.okText		||  "OK";
+	dialogSettings.continueText	=	dialogSettings.continueText	||  "Continue";
+	dialogSettings.cancelText	=	dialogSettings.cancelText	||  "Cancel";
 
-//show dialogWindow
-var elm = document.createElement("div");
-elm.id = "dialogWindow";
-document.body.appendChild(elm);
-elm.innerHTML = structure;
-var i, sec, retBool, retValue;
+	// == Switch between default values or passed values == //
+	item.title			=	item.title			||  dialogSettings.defTitle;
+	item.type			=	item.type			||  dialogSettings.defType;
+	item.content			=	item.content			||  dialogSettings.defContent;
 
-//Ovrloop over the elemens of the dialogWindow
-for (i=0; (sec = elm.getElementsByTagName("div")[i]); i++) {
+	function action(e, el) {
+		e = e || window.event;
+		var target = el || (e.target ? e.target : e.srcElement), retBool;
+		if (target.getAttribute("data-type") == "ok")
+			retBool = true;
+		if (target.getAttribute("data-type") == "cancel")
+			retBool = false;
+		if (target.getAttribute("data-type") == "ok" || target.getAttribute("data-type") == "cancel") {
+			document.body.removeChild(elm);
+			//Callback function if defined
+			if (item.callback) {
+				var returnObj = {id: item.id, action: retBool, value: retValue, vars: item.vars};
+				item.callback(returnObj);
+			}
+		}
+	}
+	
+	function useKeys(e) {
+		e = e || window.event;
+		for (var i=0; sec = elm.getElementsByTagName("div")[i]; i++) {
+			if (sec.getAttribute("data-type")=="actions") {
+				if (e.keyCode == 13) {
+					action(null, sec.children[0]);
+					document.removeEventListener("keyup", useKeys, false);
+				}
+				else if (e.keyCode == 27 && sec.children[1].style.display == "inline-block") {
+					action(null, sec.children[1]);
+					document.removeEventListener("keyup", useKeys, false);
+				}
+			}
+		}
+	}
 
-  //Setting global actions
-  if (sec.getAttribute("data-type")=="title") sec.innerHTML = item.title;
-  if (sec.getAttribute("data-type")=="text") { sec.innerHTML = item.content; sec.style.wordWrap="break-word"; }
-  
-  //Case "alert" (default)
-  if (item.type == "alert") {
-    if (sec.getAttribute("data-type")=="prompt") sec.style.display="none";
-    if (sec.getAttribute("data-type")=="actions") {
-      sec.children[0].innerHTML = "OK";
-      sec.children[1].style.display="none";
-    }
-  }
-  
-  //Case "confirm"
-  else if (item.type == "confirm") {
-    if (sec.getAttribute("data-type")=="prompt") sec.style.display="none";
-    if (sec.getAttribute("data-type")=="actions") {
-      sec.children[0].innerHTML = dialogSettings.continueText;
-      sec.children[1].style.display = "inline-block";
-    }
-  }
-  
-  //Case "prompt"
-  else if (item.type == "prompt") {
-    if (sec.getAttribute("data-type")=="prompt") {
-      sec.style.display="block";
-      var v=sec.children[0];
-      v.value = "";
-      item.defValue != null ? sec.children[0].placeholder = item.defValue : sec.children[0].placeholder = "";
-      v.focus();
-    }
-    if (sec.getAttribute("data-type")=="actions") {
-      sec.children[0].onclick=function() {retValue=v.value};
-      sec.children[0].innerHTML = "OK";
-      sec.children[1].style.display = "inline-block";
-    }
-  }
-  
-  //Generic actions of pressing buttons
-  if (sec.getAttribute("data-type")=="actions") {
-    //Add function to press enter and esc to click to ok and cancel
-    function useKeys(e) {
-      e = e || window.event;
-      for (var i=0; sec=elm.getElementsByTagName("div")[i]; i++) {
-       if (sec.getAttribute("data-type")=="actions") {
-        if (e.keyCode == 13) { sec.children[0].click(); removeEvent(document, "keyup", useKeys, false); }
-        else if (e.keyCode == 27 && sec.children[1].style.display == "inline-block") { sec.children[1].click(); removeEvent(document, "keyup", useKeys, false); }
-       }
-      }
-    }
-    addEvent(document, "keyup", useKeys, false);
-    //Listener when pressing buttons
-    addEvent(sec, "click", function(e) {
-      e = e || window.event;
-      var target = (e.target) ? e.target : e.srcElement;
-      if (target.getAttribute("data-type") == "ok") retBool=true;
-      if (target.getAttribute("data-type") == "cancel") retBool=false;
-      if (target.getAttribute("data-type") == "ok" || target.getAttribute("data-type") == "cancel") {
-       document.body.removeChild(elm);
-       //Callback function if defined
-       if (item.callBack != null) {
-        var returnObj = {id: item.id, boolean: retBool, value: retValue, vars: item.vars};
-        eval(item.callBack);
-       }
-      }
-    }, false);
-  }
+	var inputtag = document.createElement("input");
+	inputtag.setAttribute("type", "text");
+	var structure = '<div data-type="inner"><div data-type="title">Titolo del popup</div><div data-type="content"><div data-type="text">Contenuto</div><div data-type="prompt">'+inputtag.outerHTML+'</div><div data-type="actions"><button type="button" data-type="ok">'+dialogSettings.okText+'</button><button type="button" data-type="cancel">'+dialogSettings.cancelText+'</button></div></div></div>';
 
-}
+	//show dialogWindow
+	var elm = document.createElement("div");
+	elm.id = "dialogWindow";
+	document.body.appendChild(elm);
+	elm.innerHTML = structure;
+	var i, sec, retBool, retValue;
+
+	// Loop through the elements of the dialogWindow
+	for (var i=0; sec = elm.getElementsByTagName("div")[i]; i++) {
+
+	  //Setting global actions
+	  if (sec.getAttribute("data-type")=="title") sec.innerHTML = item.title;
+	  if (sec.getAttribute("data-type")=="text") { sec.innerHTML = item.content; sec.style.wordWrap="break-word"; }
+	  
+	  //Case "alert" (default)
+	  if (item.type == "alert") {
+		if (sec.getAttribute("data-type")=="prompt") sec.style.display="none";
+		if (sec.getAttribute("data-type")=="actions") {
+		  sec.children[0].innerHTML = "OK";
+		  sec.children[1].style.display="none";
+		}
+	  }
+	  
+	  //Case "confirm"
+	  else if (item.type == "confirm") {
+		if (sec.getAttribute("data-type")=="prompt") sec.style.display="none";
+		if (sec.getAttribute("data-type")=="actions") {
+		  sec.children[0].innerHTML = dialogSettings.continueText;
+		  sec.children[1].style.display = "inline-block";
+		}
+	  }
+	  
+	  //Case "prompt"
+	  else if (item.type == "prompt") {
+		if (sec.getAttribute("data-type")=="prompt") {
+		  sec.style.display="block";
+		  var v=sec.children[0];
+		  v.value = "";
+		  item.defValue ? sec.children[0].placeholder = item.defValue : sec.children[0].placeholder = "";
+		  v.focus();
+		}
+		if (sec.getAttribute("data-type")=="actions") {
+		  sec.children[0].onclick=function() {retValue=v.value};
+		  sec.children[0].innerHTML = "OK";
+		  sec.children[1].style.display = "inline-block";
+		}
+	  }
+	  
+	  //Generic actions of pressing buttons
+	  if (sec.getAttribute("data-type")=="actions") {
+		//Add function to press enter and esc to click to ok and cancel
+		document.addEventListener("keyup", useKeys, false);
+		//Listener when pressing buttons
+		sec.addEventListener("click", action, false);
+	  }
+
+	}
 
 }
